8d88998d419c395e17442f829378e0398a94a4fe
Alex Kusov <kysov2006@bk.ru>
2022-02-22 06:45:03 +0300

    play button

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import './style.css'
 
-
 const soundsElement = document.querySelector(".sounds");
 
 (async () => {
@@ -19,18 +18,26 @@ function addSounds(sounds) {
   sounds.forEach((sound) => {
     const soundDiv = document.createElement("div");
     const soundTitle = document.createElement("h2");
+    
+    soundDiv.classList.add('sound__item');
+    soundTitle.classList.add('sound__title');
+
     soundTitle.textContent = sound.title;
     soundDiv.appendChild(soundTitle);
 
     const playBtn = document.createElement("button");
     playBtn.textContent = "play";
+    playBtn.classList.add('sound__btn')
     soundDiv.appendChild(playBtn);
 
     const player = document.createElement("audio");
-    player.setAttribute("src", `assets/${sound.src}`);
+    player.setAttribute("src", `./assets/${sound.src}`);
     soundDiv.appendChild(player);
 
     playBtn.addEventListener("click", () => {
+      if(player.currentTime <= player.duration){
+        player.currentTime = 0;
+      }
       player.play();
     });
 
